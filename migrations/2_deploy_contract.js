f5d4807e007b9bc218cf6fbc1702871452ae05a1
sagarjethideveloper <sagar.jethi007@gmail.com>
2022-08-23 01:41:23 +0530

    first commit

---
+++ migrations/2_deploy_contract.js
@@ -0,0 +1,5 @@
+const Persssist = artifacts.require("Persssist");
+
+module.exports = function(_deployer) {
+  _deployer.deploy(Persssist); 
+};
---
+++ src/constants/networks.ts
@@ -0,0 +1,6 @@
+export class NetwokIds {
+    static mainnet = 1;
+    static rinkeby = 4;
+    static kovan = 42;
+    static mumbai = 80001;
+}
